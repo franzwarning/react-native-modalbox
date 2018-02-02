8dcae98ff8b4a10ff6d18e5f0c54eccba9d505ea
Raymond Kennedy <george@getjotdown.com>
2018-02-02 10:27:50 -0800

    Commenting out keyboard.dismiss

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ var ModalBox = createReactClass({
       }
     );
     this.state.animClose.start(() => {
-      Keyboard.dismiss();
+      //Keyboard.dismiss();
       this.state.isAnimateClose = false;
       this.state.isOpen = false;
       this.setState({});
